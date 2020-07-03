3666de2cd210893d92c283b42a2b7176890b8567
Jarosław Czub <your_email@abc.com>
2020-07-03 08:05:31 +0200

    Add CS3 file API

--- cs3api_test_ext/handlers.py
+++ cs3api_test_ext/handlers.py
@@ -4,6 +4,9 @@ from notebook.base.handlers import APIHandler
 from tornado import gen, web
 import json
 
+from cs3api_test_ext import CS3APIsManager
+
+
 class HelloWorldHandle(APIHandler):
 
     def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
@@ -24,11 +27,28 @@ class FilesHandle(APIHandler):
     def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
         pass
 
+    @property
+    def cs3api_manager(self):
+        # return self.settings['cs3api_manager']
+        return CS3APIsManager()
+
     @web.authenticated
     @gen.coroutine
-    def get(self):
+    def get(self, path=""):
+
+        type = self.get_query_argument('type', default=None)
+
+        format = self.get_query_argument('format', default=None)
+
+        content = self.get_query_argument('content', default='1')
+
+        cs3manager = self.cs3api_manager
         output = {
-            'files': 'files'
+            'files': 'files',
+            'path': path,
+            'format': format,
+            'type': type,
+            'model': cs3manager.get(path=path, type=type, format=format, content=content)
         }
         self.set_header('Content-Type', 'application/json')
         self.finish(json.dumps(output))
