a90093c692b09bc45d6b04c70899855f721fbba0
magc <magc@ailleron.com>
2020-07-15 17:58:29 +0200

    create share method test impl

--- cs3api_test_ext/tests/test_cs3_share_api.py
+++ cs3api_test_ext/tests/test_cs3_share_api.py
@@ -1,21 +1,43 @@
 from unittest import TestCase
 
+import http
+import time
+
+import grpc
+import cs3.gateway.v1beta1.gateway_api_pb2 as gateway
+import cs3.gateway.v1beta1.gateway_api_pb2_grpc as grpc_gateway
+import cs3.sharing.collaboration.v1beta1.collaboration_api_pb2 as sharing
+import cs3.sharing.collaboration.v1beta1.resources_pb2 as sharing_res
+import cs3.sharing.collaboration.v1beta1.collaboration_api_pb2_grpc as sharing_grpc
+import cs3.sharing.collaboration.v1beta1.resources_pb2 as sh
+# import cs3.sharing.ocm.v1beta1 as sh
+from cs3api_test_ext.cs3_share_api import Cs3ShareApi
+
 
 class TestCs3ShareApi(TestCase):
-	def test_create(self):
-		self.fail()
+    api = Cs3ShareApi()
+    config = {
+        "revahost": "127.0.0.1:19000",
+        "authtokenvalidity": 3600,
+        "userid": "einstein",
+        "endpoint": "/",
+        "chunksize": 4194304
+    }
 
-	def test_list(self):
-		self.fail()
+    def test_create(self):
+        self.api.create("/", "/test.txt", "einstein", "marie")
 
-	def test_remove(self):
-		self.fail()
+    def test_list(self):
+        self.fail()
 
-	def test_update(self):
-		self.fail()
+    def test_remove(self):
+        self.fail()
 
-	def test_list_received(self):
-		self.fail()
+    def test_update(self):
+        self.fail()
 
-	def test_update_received(self):
-		self.fail()
+    def test_list_received(self):
+        self.fail()
+
+    def test_update_received(self):
+        self.fail()
