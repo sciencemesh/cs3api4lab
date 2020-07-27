8f976e1f104bf09255eec325c883c554529dcfb5
Jarosław Czub <your_email@abc.com>
2020-07-27 13:54:52 +0200

    Implements read directory model for "/api/contents/" request

--- cs3api4lab/cs3apismanager.py
+++ cs3api4lab/cs3apismanager.py
@@ -1,6 +1,48 @@
+from datetime import datetime
+
+from jupyter_core.paths import jupyter_config_path
+from notebook.services.config import ConfigManager
 from notebook.services.contents.manager import ContentsManager
+from tornado import web
+
+from cs3api4lab.cs3_file_api import Cs3FileApi
+from notebook import _tz as tz
+import mimetypes
 
 class CS3APIsManager(ContentsManager):
+    cs3_config_dir = ""
+    cs3_config = {}
+    cs3_user_id = "einstein"
+
+    # ToDo: Change to cs3 Type
+    TYPE_FILE = 1
+    TYPE_DIRECTORY = 2
+
+    log = None
+    cs3_endpoint = None
+
+    def __init__(self, parent, log):
+
+        #
+        # Get config from jupyter_cs3_config.json file
+        #
+        config_path = jupyter_config_path()
+        if self.cs3_config_dir not in config_path:
+            # add self.config_dir to the front, if set manually
+            config_path.insert(0, self.cs3_config_dir)
+        cm = ConfigManager(read_config_path=config_path)
+
+        cs3_config = cm.get('jupyter_cs3_config')
+
+        self.cs3_config = cs3_config.get("cs3", {
+            "revahost": "127.0.0.1:19000",
+            "endpoint": "/",
+            "authtokenvalidity": "3600",
+            "chunksize": "4194304"
+        })
+
+        self.cs3_endpoint = self.cs3_config["endpoint"]
+        self.log = log
 
     def dir_exists(self, path):
         """Does a directory exist at the given path?
@@ -47,8 +89,20 @@ class CS3APIsManager(ContentsManager):
         raise NotImplementedError('cs3: missing')
 
     def get(self, path, content=True, type=None, format=None):
-        """Get a file or directory model."""
-        raise NotImplementedError('cs3: missing')
+        """Get a file, notebook or directory model."""
+
+        path = self._normalize_path(path)
+
+        if type in (None, 'directory') and self._is_dir(path):
+            model = self._dir_model(path, content=content)
+        elif type == 'notebook' or (type is None and path.endswith('.ipynb')):
+            raise NotImplementedError('cs3: missing')
+        else:
+            if type == 'directory':
+                raise web.HTTPError(400, u'%s is a directory' % path, reason='bad type')
+            raise NotImplementedError('cs3: missing')
+
+        return model
 
     def save(self, model, path):
         """
@@ -69,3 +123,33 @@ class CS3APIsManager(ContentsManager):
 
     
 
+                    if type == 'notebook' or (type is None and path.endswith('.ipynb')):
+                        contents.append(
+                            self._convert_container_to_notebook_model(cs3_model, cs3_container)
+                        )
+                    else:
+                        contents.append(
+                            self._convert_container_to_file_model(cs3_model, cs3_container)
+                        )
+                else:
+
+                    self.log.error(u'Unexpected type: %s %s', cs3_model.path, cs3_model.type)
+                    raise web.HTTPError(500, u'Unexpected type: %s %s' % (cs3_model.path, cs3_model.type))
+
+        return model
+
+    def _convert_container_to_file_model(self, cs3_model, cs3_container):
+
+        model = self._convert_container_to_base_model(cs3_model.path, cs3_container)
+        model['type'] = 'file'
+        model['mimetype'] = mimetypes.guess_type(cs3_model.path)[0]
+
+        return model
+
+    def _convert_container_to_notebook_model(self, cs3_model, cs3_container):
+
+        model = self._convert_container_to_base_model(cs3_model.path, cs3_container)
+        model['type'] = 'notebook'
+        model['mimetype'] = mimetypes.guess_type(cs3_model.path)[0]
+
+        return model
