8a0b365f2229c922752ad3764636caaff0af8f0e
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2020-07-28 09:21:21 +0200

    master: fix typo

--- src/services/findFileIcon.ts
+++ src/services/findFileIcon.ts
@@ -4,11 +4,11 @@ import {DocumentRegistry} from "@jupyterlab/docregistry";
 
 export const findFileIcon = (fileInfo :Contents.IModel) :LabIcon => {
     let splitName = fileInfo.name.split('.');
-    let fileExtention = '.' + splitName[splitName.length-1];
+    let fileExtension = '.' + splitName[splitName.length-1];
 
     let fileType = DocumentRegistry.defaultFileTypes
         .filter( (fileType: Partial<DocumentRegistry.IFileType>) => {
-            return fileType.contentType == "directory" || fileType.extensions.lastIndexOf(fileExtention) >= 0;
+            return fileType.contentType == "directory" || fileType.extensions.lastIndexOf(fileExtension) >= 0;
         });
 
     return (fileType.length > 0 ) ? fileType[0].icon : LabIcon.resolve({
