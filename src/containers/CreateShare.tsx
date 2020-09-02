ebc9c00b151939151cbcf6e763567b59a7ff9885
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2020-09-02 13:53:13 +0200

    filemanager: small fixes

--- src/containers/CreateShare.tsx
+++ src/containers/CreateShare.tsx
@@ -18,7 +18,7 @@ const CreateShare = (): JSX.Element => {
     return (<>
         <ControlPanel makeRequest={async () => {
             try {
-                const data = await requestAPI<any>('/api/cs3test/shares?endpoint=%2F&file_id=%2Fexample1.txt&grantee=f7fbf8c8-139b-4376-b307-cf0a8c2d0d9c&idp=cesnet.cz&role=viewer&grantee_type=user&token=92c172723a1121bc740b4ebac65e9047881ae82f503a15c5', {
+                const data = await requestAPI<any>('/api/cs3test/shares?endpoint=/&file_id=/example1.txt&grantee=f7fbf8c8-139b-4376-b307-cf0a8c2d0d9c&idp=cesnet.cz&role=viewer&grantee_type=user&token=92c172723a1121bc740b4ebac65e9047881ae82f503a15c5', {
                     method: 'POST',
                     // body: JSON.stringify({
                     //     endpoint: '/',
