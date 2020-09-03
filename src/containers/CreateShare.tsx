ca4f5bdbe7892bb4b11462c4938456ac9f0d4f28
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2020-09-03 15:37:17 +0200

    filemanager: modal with working crete share based on file chosen from context menu in filemanager

--- src/containers/CreateShare.tsx
+++ src/containers/CreateShare.tsx
@@ -6,35 +6,37 @@ import {requestAPI} from "../services/requestAPI";
 // components
 import ControlPanel from "../components/ControlPanel";
 import Results from "../components/Results";
+import {Contents} from "@jupyterlab/services";
+
+
+type CreateShareProps = {
+    fileInfo: Contents.IModel;
+}
 
 /**
  * Main container.
  *
  * @constructor
  */
-const CreateShare = (): JSX.Element => {
+const CreateShare = (props: CreateShareProps): JSX.Element => {
     const [message, setMessage] = useState(null);
 
     return (<>
-        <ControlPanel makeRequest={async () => {
-            try {
-                const data = await requestAPI<any>('/api/cs3test/shares?endpoint=/&file_id=/example1.txt&grantee=f7fbf8c8-139b-4376-b307-cf0a8c2d0d9c&idp=cesnet.cz&role=viewer&grantee_type=user&token=92c172723a1121bc740b4ebac65e9047881ae82f503a15c5', {
-                    method: 'POST',
-                    // body: JSON.stringify({
-                    //     endpoint: '/',
-                    //     file_id: 'example1.txt',
-                    //     grantee: 'f7fbf8c8-139b-4376-b307-cf0a8c2d0d9c',
-                    //     idp: 'cesnet.cz',
-                    //     role: 'viewer',
-                    //     gratee_type: 'user',
-                    // })
-                });
+        <ControlPanel fileInfo={props.fileInfo}
+                      makeRequest={async (params: object) => {
+                          try {
+                              const data = await requestAPI<any>('/api/cs3test/shares', {
+                                  method: 'POST',
+                                  body: JSON.stringify(params)
+                              });
 
-                setMessage(JSON.stringify(data));
-            } catch (e) {
-                console.log('request errors', e);
-            }
-        }}/>
+                              setMessage(JSON.stringify(data));
+                          } catch (e) {
+                              console.log('request errors', e);
+                          }
+                      }}
+
+        />
 
         <Results message={message} />
     </>);
