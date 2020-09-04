3d8efca622c2ac4afc51126c303ebffba9b05b03
magc <magc@ailleron.com>
2020-09-04 15:23:11 +0200

    extracted authenticator

---
+++ cs3api_test_ext/authenticator.py
@@ -0,0 +1,29 @@
+import cs3.gateway.v1beta1.gateway_api_pb2 as cs3gw
+import time
+import cs3.gateway.v1beta1.gateway_api_pb2_grpc as cs3gw_grpc
+
+
+class Authenticator:
+    tokens = {}  # map user_id [string] to {authentication token, token expiration time}
+
+    def __init__(self, config, channel):
+        self.config = config
+        self.cs3_stub = cs3gw_grpc.GatewayAPIStub(channel)
+
+    def authenticate(self, user_id):
+        # ToDo: use real authentication data or get token from author provider
+        # authReq = cs3gw.AuthenticateRequest(type='bearer', client_secret=userid)
+
+        if user_id not in self.tokens or self.tokens[user_id]['exp'] < time.time():
+            auth_req = cs3gw.AuthenticateRequest(type=self.config['login_type'],
+                                                 client_id=self.config['client_id'],
+                                                 client_secret=self.config['client_secret'])
+            auth_res = self.cs3_stub.Authenticate(auth_req)
+            self.tokens[user_id] = {'tok': auth_res.token,
+                                    'exp': time.time() + float(self.config['auth_token_validity'])}
+
+        # piggy back on the opportunity to expire old tokens, but asynchronously
+        # as to not impact the current session: let's use python3.7's coroutines support
+        # asyncio.run(_async_cleanuptokens())
+
+        return self.tokens[user_id]['tok']
