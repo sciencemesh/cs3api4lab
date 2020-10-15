a0f49c2618c8b54610b54f8a12ee5edc8fcd4b96
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2020-10-15 09:43:11 +0200

    filemanager: simple file model from backend

--- src/CS3Drive.ts
+++ src/CS3Drive.ts
@@ -1,41 +1,13 @@
-// import {Contents} from "@jupyterlab/services";
 import {requestAPI} from './services/requestAPI';
 import {IStateDB} from '@jupyterlab/statedb';
 import { ReadonlyJSONObject } from '@lumino/coreutils';
 
-export async function CS3ContainerFiles(stateDB: IStateDB, path: string = null) :Promise<any> {
+export async function getDummyFilesForCS3Share(stateDB: IStateDB) :Promise<any> {
     let share = await stateDB.fetch('share');
     const shareType = (share as ReadonlyJSONObject)['share_type']
 
-    let fileList = null;
-    switch (shareType) {
-        case 'by_me':
-            fileList = await getSharedByMe();
-            break;
-        case 'with_me':
-            fileList = await getSharedWithMe();
-            break;
-        case 'filelist':
-        default:
-            fileList = await getFileList(path);
-    }
-
-    return fileList;
-}
-
-export async function CS3Rename(oldLocalPath: string, newLocalPath: string) {
-    // requestAPI('/api/contents/' + oldLocalPath, {
-    //     method: 'PATCH',
-    //     body: {
-    //         path: newLocalPath
-    //     }
-    // })
-}
-
-async function getFileList(path: string): Promise<any> {
-    return await requestAPI('/api/contents/' + path, {
-        method: 'get',
-    });
+    const shares = (shareType == 'with_me') ? await getSharedWithMe() : await getSharedByMe();
+    return shares;
 }
 
 async function getSharedByMe(): Promise<any>{
