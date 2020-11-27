94d6925fef8f3c758b701ff2063be47e18c294f9
Jarosław Czub <your_email@abc.com>
2020-11-27 12:44:53 +0100

    Fix download and upload after changes in cs3api

--- cs3api4lab/api/cs3_share_api.py
+++ cs3api4lab/api/cs3_share_api.py
@@ -82,10 +82,8 @@ class Cs3ShareApi:
                                                 metadata=[('x-access-token', self.get_token())])
         if self._is_code_ok(list_response):
             self.log.info("List shares response for user: " + self.config['client_id'])
-            self.log.info(list_response)
         else:
             self.log.error("Error listing shares response for user: " + self.config['client_id'])
-            self._handle_error(list_response)
         return list_response
 
     def list_grantees_for_file(self, file_path):
