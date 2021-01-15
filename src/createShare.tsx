433a90e25ea1dc4b89e7aeb11efcc86d4e49304d
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2021-01-15 13:04:08 +0100

    filemanger: refactor to fit jupyter lab plugin standards

--- src/createShare.tsx
+++ src/createShare.tsx
@@ -51,7 +51,7 @@ const ShareForm = (props: ShareFormProps): JSX.Element => {
     }
 
     return (
-        <div className='ShareForm'>
+        <div className='controlPanel'>
             <div>
                 <div>Grantee</div>
                 <div>
