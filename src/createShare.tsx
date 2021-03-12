75940e6c3f1b9c951da6f45438a3c6fc5fb1a870
Jarosław Czub <your_email@abc.com>
2021-03-12 07:51:51 +0100

    Add basic tabs layout

--- src/createShare.tsx
+++ src/createShare.tsx
@@ -1,9 +1,15 @@
-import {ReactWidget} from '@jupyterlab/apputils';
-import React, {useState} from 'react';
-import {Contents} from "@jupyterlab/services";
-import {requestAPI} from "./services";
-import {CreateShareProps, ResultProps, ShareFormProps, WidgetProps} from "./types";
-
+import { ReactWidget } from '@jupyterlab/apputils';
+import React, { useEffect, useState } from 'react';
+import { Contents } from '@jupyterlab/services';
+import { requestAPI } from './services';
+import {
+  CreateShareProps,
+  ResultProps,
+  ShareFormProps,
+  UsersRequest,
+  WidgetProps
+} from './types';
+import Select from 'react-dropdown-select';
 
 /**
  * Request results panel.
@@ -13,15 +19,12 @@ import {CreateShareProps, ResultProps, ShareFormProps, WidgetProps} from "./type
  * @constructor
  */
 const Results = (props: ResultProps): JSX.Element => {
-    return (
-        <div className='resultWindow'>
-            <pre>
-                {props.message}
-            </pre>
-        </div>
-    );
-}
-
+  return (
+    <div className="resultWindow">
+      <pre>{props.message}</pre>
+    </div>
+  );
+};
 
 /**
  * Share form widget.
@@ -30,65 +33,107 @@ const Results = (props: ResultProps): JSX.Element => {
  *
  * @constructor
  */
-const ShareForm = (props: ShareFormProps): JSX.Element => {
-    const [formValues, setFormState] = useState({
-        endpoint: '/',
-        file_path: '/home/' + props.fileInfo.path.replace('cs3drive:', ''),
-        grantee: '',
-        idp: '',
-        role: 'viewer',
-        grantee_type: 'user'
+const ShareForm: React.FC<ShareFormProps> = (
+  props: ShareFormProps
+): JSX.Element => {
+  const [userList, setUserList] = useState([{}]);
+  const [selectedUser, setSelectedUser] = useState([
+    {
+      idp: '',
+      grantee: ''
+    }
+  ]);
+
+  const [formValues, setFormState] = useState({
+    endpoint: '/',
+    file_path: '/home/' + props.fileInfo.path.replace('cs3drive:', ''),
+    grantee: '',
+    idp: '',
+    role: 'viewer',
+    grantee_type: 'user'
+  });
+
+  const setValues = (selectValues: Array<any>) => {
+    setSelectedUser(selectValues);
+  };
+
+  useEffect(() => {
+    props.getUsers('').then(users => {
+      const parsedUsers: Array<object> = [];
+
+      let i = 1;
+      for (const user of users) {
+        parsedUsers.push({
+          id: i++,
+          name: user.display_name,
+          displayName: user.display_name,
+          idp: user.idp,
+          grantee: user.opaque_id
+        });
+      }
+      setUserList(parsedUsers);
     });
+  }, []);
 
-    const setFormStateFromValues = (param: React.ChangeEvent<HTMLInputElement>|React.ChangeEvent<HTMLSelectElement>) => {
-        let tmpFormState :any = {...formValues};
-        tmpFormState[param.target.name] = param.target.value;
-        setFormState(tmpFormState);
-    }
+  const setFormStateFromValues = (
+    param:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const tmpFormState: any = { ...formValues };
+    tmpFormState[param.target.name] = param.target.value;
+    setFormState(tmpFormState);
+  };
 
-    const localMakeRequest = () => {
-        props.makeRequest(formValues)
-    }
+  const localMakeRequest = () => {
+    const [user] = [...selectedUser];
+    const _formValues = { ...formValues };
 
-    return (
-        <div className='ShareForm'>
-            <div>
-                <div>Grantee</div>
-                <div>
-                    <input type='text' value={formValues.grantee} name='grantee' onChange={setFormStateFromValues}/>
-                </div>
-            </div>
-            <div>
-                <div>IDP</div>
-                <div>
-                    <input type='text' value={formValues.idp} name='idp' onChange={setFormStateFromValues} />
-                </div>
-            </div>
+    _formValues.idp = user.idp;
+    _formValues.grantee = user.grantee;
 
-            <div>
-                <div>Role</div>
-                <div>
-                    <select onChange={setFormStateFromValues} name='role'>
-                        <option value='viewer'>Viewer</option>
-                        <option value='editor'>Editor</option>
-                    </select>
-                </div>
-            </div>
+    props.makeRequest(_formValues);
+  };
 
-            {/*<div>*/}
-            {/*    <div>Grantee Type</div>*/}
-            {/*    <div>*/}
-            {/*        <select name='grantee_type' id='grantee_type' onChange={setFormStateFromValues}>*/}
-            {/*            <option value='user'>User</option>*/}
-            {/*            <option value="group">Group</option>*/}
-            {/*        </select>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
-            <button onClick={localMakeRequest}>Make request</button>
+  return (
+    <div className="ShareForm">
+      <div>
+        <div>Grantee</div>
+        <div>
+          <Select
+            searchable={true}
+            options={userList}
+            values={[]}
+            valueField="name"
+            labelField="displayName"
+            onChange={setValues}
+          />
         </div>
-    )
-}
+      </div>
+      <div>
+        <div>Role</div>
+        <div>
+          <select onChange={setFormStateFromValues} name="role">
+            <option value="viewer">Viewer</option>
+            <option value="editor">Editor</option>
+          </select>
+        </div>
+      </div>
+
+      {/*<div>*/}
+      {/*    <div>Grantee Type</div>*/}
+      {/*    <div>*/}
+      {/*        <select name='grantee_type' id='grantee_type' onChange={setFormStateFromValues}>*/}
+      {/*            <option value='user'>User</option>*/}
+      {/*            <option value="group">Group</option>*/}
+      {/*        </select>*/}
+      {/*    </div>*/}
+      {/*</div>*/}
+
+      <button onClick={localMakeRequest}>Make request</button>
+    </div>
+  );
+};
 
 /**
  * Main container.
@@ -96,49 +141,46 @@ const ShareForm = (props: ShareFormProps): JSX.Element => {
  * @constructor
  */
 const CreateShare = (props: CreateShareProps): JSX.Element => {
-    const [message, setMessage] = useState(null);
+  const [message, setMessage] = useState(null);
 
-    return (<>
-        <ShareForm fileInfo={props.fileInfo}
-                      makeRequest={async (params: object) => {
-                          try {
-                              const data = await requestAPI<any>('/api/cs3/shares', {
-                                  method: 'POST',
-                                  body: JSON.stringify(params)
-                              });
+  return (
+    <>
+      <ShareForm
+        fileInfo={props.fileInfo}
+        getUsers={async (query): Promise<Array<UsersRequest>> => {
+          return await requestAPI('api/cs3/user/query?query=' + query, {});
+        }}
+        makeRequest={async (params: object) => {
+          try {
+            const data = await requestAPI<any>('/api/cs3/shares', {
+              method: 'POST',
+              body: JSON.stringify(params)
+            });
 
-                              setMessage(JSON.stringify(data));
-                          } catch (e) {
-                              console.log('request errors', e);
-                          }
-                      }}
+            setMessage(JSON.stringify(data));
+          } catch (e) {
+            console.log('request errors', e);
+          }
+        }}
+      />
 
-        />
-
-        <Results message={message} />
-    </>);
+      <Results message={message} />
+    </>
+  );
 };
-
 
 /**
  * ShareWidget container.
  */
 export class ShareWidget extends ReactWidget {
-    private readonly fileInfo: Contents.IModel;
+  private readonly fileInfo: Contents.IModel;
+  constructor(props: WidgetProps) {
+    super();
+    this.addClass('jp-ReactWidget');
+    this.fileInfo = props.fileInfo;
+  }
 
-    constructor(props :WidgetProps) {
-        super();
-        this.addClass('jp-ReactWidget');
-        this.fileInfo = props.fileInfo;
-    }
-
-    protected render(): JSX.Element {
-        return (
-            <CreateShare fileInfo={this.fileInfo}/>
-        )
-    }
+  protected render(): JSX.Element {
+    return <CreateShare fileInfo={this.fileInfo} />;
+  }
 }
-
-
-
-
--- src/types.ts
+++ src/types.ts
@@ -1,38 +1,55 @@
-import {Contents} from "@jupyterlab/services";
+import { Contents } from '@jupyterlab/services';
 
 export type ResultProps = {
-    message: string;
-}
+  message: string;
+};
 export type ShareFormProps = {
-    makeRequest: (params: object) => void;
-    fileInfo: Contents.IModel;
-}
+  makeRequest: (params: object) => void;
+  getUsers: (query: string) => Promise<Array<UsersRequest>>;
+  fileInfo: Contents.IModel;
+};
 export type CreateShareProps = {
-    fileInfo: Contents.IModel;
-}
+  fileInfo: Contents.IModel;
+};
 export type WidgetProps = {
-    fileInfo: Contents.IModel
-}
+  fileInfo: Contents.IModel;
+};
 export type MainProps = {
-    fileInfo: Contents.IModel
-}
+  fileInfo: Contents.IModel;
+};
 export type MenuProps = {
-    tabHandler: (tabname: string) => void
-}
+  tabHandler: (tabname: string) => void;
+};
 export type ContentProps = {
-    content: Contents.IModel,
-    contentType: string,
-    grantees: Map<string, string>
-}
+  content: Contents.IModel;
+  contentType: string;
+  // getGrantees: Promise<Map<string, string>>
+  // grantees: Map<string, string>
+};
 export type HeaderProps = {
-    fileInfo: Contents.IModel
-}
+  fileInfo: Contents.IModel;
+};
 export type ShareProps = {
-    fileInfo: Contents.IModel,
-}
+  fileInfo: Contents.IModel;
+};
 export type InfoProps = {
-    content: Contents.IModel
-}
+  content: Contents.IModel;
+};
 export type SharesProps = {
-    grantees: Map<string, string>
-}
+  // grantees: Map<string, string>,
+  content: Contents.IModel;
+};
+
+export type UsersRequest = {
+  display_name: string;
+  name: string;
+  idp: string;
+  opaque_id: string;
+};
+
+export type User = {
+  displayName: string;
+  name: string;
+  idp: string;
+  opaqueId: string;
+};
