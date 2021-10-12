c27f5d1bbd57d7be95c61a37db2c484281f9f3b1
dagl <dagl@softwaremind.com>
2021-10-12 23:14:44 +0200

    Update cs3api4lab/utils/file_utils.py
    Co-authored-by: Diogo Castro <diogo.castro@cern.ch>

--- cs3api4lab/utils/file_utils.py
+++ cs3api4lab/utils/file_utils.py
@@ -20,7 +20,7 @@ class FileUtils:
     @staticmethod
     def _check_and_transform_file_path(file_id): 
 
-        config = Cs3ConfigManager.get_config() #note: can cause problems in tests because of the config, it should be passed as and argument
+        config = Cs3ConfigManager.get_config() #note: can cause problems in tests because of the config, it should be passed as an argument
         has_root_dir = file_id.startswith(config["root_dir_list"])
 
         if len(config["home_dir"]) > 0 and not file_id.startswith(config["home_dir"]) and not has_root_dir:
