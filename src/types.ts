fb568459f3e870e6fed136842b53dfa93b458094
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2021-12-06 15:25:16 +0100

    eslint: fix line endings and small corrections to the code
    eslint: fixing linter problems - tabs,jsdoc comments, return types etc

--- src/types.ts
+++ src/types.ts
@@ -1,69 +1,52 @@
-import {Contents} from '@jupyterlab/services';
-import {IStateDB} from "@jupyterlab/statedb";
-import {FileBrowser} from "@jupyterlab/filebrowser";
-import {WidgetTracker} from "@jupyterlab/apputils";
+import { Contents } from '@jupyterlab/services';
+import { IStateDB } from '@jupyterlab/statedb';
+import { FileBrowser } from '@jupyterlab/filebrowser';
+import { WidgetTracker } from '@jupyterlab/apputils';
 
-export type ResultProps = {
-    message: string;
-};
 export type ShareFormProps = {
-    makeRequest: (params: object) => void;
-    getUsers: (query: string) => Promise<Array<UsersRequest>>;
-    fileInfo: Contents.IModel;
+  makeRequest: (params: any) => void;
+  getUsers: (query: string) => Promise<Array<UsersRequest>>;
+  fileInfo: Contents.IModel;
 };
 export type CreateShareProps = {
-    fileInfo: Contents.IModel;
-    widgetTracker: WidgetTracker;
+  fileInfo: Contents.IModel;
+  widgetTracker: WidgetTracker;
 };
-export type WidgetProps = {
-    fileInfo: Contents.IModel;
-};
+
 export type MainProps = {
-    fileInfo: Contents.IModel;
-    widgetTracker: WidgetTracker;
+  fileInfo: Contents.IModel;
+  widgetTracker: WidgetTracker;
 };
 export type MenuProps = {
-    tabHandler: (tabname: string) => void;
+  tabHandler: (tabname: string) => void;
 };
 export type ContentProps = {
-    content: Contents.IModel;
-    contentType: string;
-    widgetTracker: WidgetTracker;
-    // getGrantees: Promise<Map<string, string>>
-    // grantees: Map<string, string>
+  content: Contents.IModel;
+  contentType: string;
+  widgetTracker: WidgetTracker;
+  // getGrantees: Promise<Map<string, string>>
+  // grantees: Map<string, string>
 };
 export type HeaderProps = {
-    fileInfo: Contents.IModel;
+  fileInfo: Contents.IModel;
 };
 export type ShareProps = {
-    fileInfo: Contents.IModel;
-    widgetTracker: WidgetTracker
+  fileInfo: Contents.IModel;
+  widgetTracker: WidgetTracker;
 };
 export type InfoProps = {
-    content: Contents.IModel;
-};
-export type SharesProps = {
-    // grantees: Map<string, string>,
-    content: Contents.IModel;
+  content: Contents.IModel;
 };
 
 export type BottomProps = {
-    db: IStateDB;
-    browser: FileBrowser
-}
-
-export type UsersRequest = {
-    display_name: string;
-    name: string;
-    idp: string;
-    opaque_id: string;
-    permission: string;
+  db: IStateDB;
+  browser: FileBrowser;
 };
 
-export type User = {
-    displayName: string;
-    name: string;
-    idp: string;
-    opaqueId: string;
-    permission: string;
+export type UsersRequest = {
+  display_name: string;
+  name: string;
+  idp: string;
+  opaque_id: string;
+  permission: string;
 };
