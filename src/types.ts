3956b6ff9e0e0d6c2ae5bb6991b823e368ef8884
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2022-01-17 07:23:29 +0100

    eslint changes

--- src/types.ts
+++ src/types.ts
@@ -1,64 +1,62 @@
-import {Contents} from '@jupyterlab/services';
-import {IStateDB} from "@jupyterlab/statedb";
-import {FileBrowser} from "@jupyterlab/filebrowser";
+import { Contents } from '@jupyterlab/services';
+import { IStateDB } from '@jupyterlab/statedb';
+import { FileBrowser } from '@jupyterlab/filebrowser';
 
 export type ResultProps = {
-    message: string;
+  message: string;
 };
 export type ShareFormProps = {
-    makeRequest: (params: object) => void;
-    getUsers: (query: string) => Promise<Array<UsersRequest>>;
-    fileInfo: Contents.IModel;
+  makeRequest: (params: any) => void;
+  getUsers: (query: string) => Promise<Array<UsersRequest>>;
+  fileInfo: Contents.IModel;
 };
 export type CreateShareProps = {
-    fileInfo: Contents.IModel;
+  fileInfo: Contents.IModel;
 };
 export type WidgetProps = {
-    fileInfo: Contents.IModel;
+  fileInfo: Contents.IModel;
 };
 export type MainProps = {
-    fileInfo: Contents.IModel;
+  fileInfo: Contents.IModel;
 };
 export type MenuProps = {
-    tabHandler: (tabname: string) => void;
+  tabHandler: (tabname: string) => void;
 };
 export type ContentProps = {
-    content: Contents.IModel;
-    contentType: string;
-    // getGrantees: Promise<Map<string, string>>
-    // grantees: Map<string, string>
+  content: Contents.IModel;
+  contentType: string;
 };
 export type HeaderProps = {
-    fileInfo: Contents.IModel;
+  fileInfo: Contents.IModel;
 };
 export type ShareProps = {
-    fileInfo: Contents.IModel;
+  fileInfo: Contents.IModel;
 };
 export type InfoProps = {
-    content: Contents.IModel;
+  content: Contents.IModel;
 };
 export type SharesProps = {
-    // grantees: Map<string, string>,
-    content: Contents.IModel;
+  // grantees: Map<string, string>,
+  content: Contents.IModel;
 };
 
 export type BottomProps = {
-    db: IStateDB;
-    browser: FileBrowser
-}
+  db: IStateDB;
+  browser: FileBrowser;
+};
 
 export type UsersRequest = {
-    display_name: string;
-    name: string;
-    idp: string;
-    opaque_id: string;
-    permission: string;
+  display_name: string;
+  name: string;
+  idp: string;
+  opaque_id: string;
+  permission: string;
 };
 
 export type User = {
-    displayName: string;
-    name: string;
-    idp: string;
-    opaqueId: string;
-    permission: string;
+  displayName: string;
+  name: string;
+  idp: string;
+  opaqueId: string;
+  permission: string;
 };
