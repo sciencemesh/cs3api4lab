e1c3e16910e49973990166300d903618dd99045a
Piotr Wichliński <piotr.wichlinski@softwaremind.com>
2022-02-07 13:55:57 +0100

    small code refactor, fixed share information box, cs3 info is renamed to file info, create share is renamed to public links

--- src/services.ts
+++ src/services.ts
@@ -60,3 +60,25 @@ export async function requestAPI<T>(
 
   return data;
 }
+
+/**
+ * Format bytes to human readable string.
+ */
+export function formatFileSize(
+  bytes: number,
+  decimalPoint: number,
+  k: number
+): string {
+  // https://www.codexworld.com/how-to/convert-file-size-bytes-kb-mb-gb-javascript/
+  if (bytes === 0) {
+    return '0 Bytes';
+  }
+  const dm = decimalPoint || 2;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  if (i >= 0 && i < sizes.length) {
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  } else {
+    return String(bytes);
+  }
+}
