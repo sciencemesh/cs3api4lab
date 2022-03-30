395afb3edb280bb5db2d4e07b2014cf3c791f887
Dawid Golosz <dagl@softwaremind.com>
2022-03-30 14:44:05 +0200

    cleanup

--- cs3api4lab/tests/test_cs3_user_api.py
+++ cs3api4lab/tests/test_cs3_user_api.py
@@ -1,20 +1,15 @@
 import unittest
 from unittest import TestCase
+
 from cs3api4lab.config.config_manager import Cs3ConfigManager
 from cs3api4lab.api.cs3_user_api import Cs3UserApi
 from traitlets.config import LoggingConfigurable
 
 
 class TestCs3UserApi(TestCase, LoggingConfigurable):
-    client_id = None
-    endpoint = None
 
     def setUp(self):
         config = Cs3ConfigManager.get_config()
-        self.client_id = config['client_id']
-        self.endpoint = config['endpoint']
-
-
         self.user_api = Cs3UserApi(self.log)
 
     def test_get_user_info(self):
@@ -39,9 +34,27 @@ class TestCs3UserApi(TestCase, LoggingConfigurable):
 
         self.assertFalse(user_info)
 
+    def test_get_user_info_empty_id(self):
+        opaque_id = ""
+        idp = "cernbox.cern.ch"
+
+        user_info = self.user_api.get_user_info(idp, opaque_id)
+
+        self.assertFalse(user_info)
+
     def test_get_user_by_claim(self):
-        claim = "eintein"
-        value = "cernbox.cern.ch"
+        claim = "mail"
+        value = "einstein@cern.ch"
+
+        expected_info = {"username": "einstein",
+                    "display_name": "Albert Einstein",
+                    "idp": "cernbox.cern.ch",
+                    "opaque_id": "4c510ada-c86b-4815-8820-42cdf82c3d51",
+                    "mail": "einstein@cern.ch"}
+
+        user_info = self.user_api.get_user_info_by_claim(claim, value)
+
+        self.assertDictEqual(user_info, expected_info)
 
     def test_find_users_by_query(self):
         query = 'einstein'
@@ -59,6 +72,9 @@ class TestCs3UserApi(TestCase, LoggingConfigurable):
         self.assertDictEqual(user_info, expected_info)
 
     def test_find_users_by_query_less_than_three_chars(self):
+        query = ''
+        self.assertFalse(self.user_api.find_users_by_query(query))
+
         query = 'e'
         self.assertFalse(self.user_api.find_users_by_query(query))
 
@@ -79,4 +95,3 @@ class TestCs3UserApi(TestCase, LoggingConfigurable):
 
         user_info = user[0]
         self.assertDictEqual(user_info, expected_info)
-
