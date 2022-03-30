6f1a618b28cdab9eafcba0cee61a39c6b6e69957
Dawid Golosz <dagl@softwaremind.com>
2022-03-30 14:39:37 +0200

    add user api tests

---
+++ cs3api4lab/tests/test_cs3_user_api.py
@@ -0,0 +1,82 @@
+import unittest
+from unittest import TestCase
+from cs3api4lab.config.config_manager import Cs3ConfigManager
+from cs3api4lab.api.cs3_user_api import Cs3UserApi
+from traitlets.config import LoggingConfigurable
+
+
+class TestCs3UserApi(TestCase, LoggingConfigurable):
+    client_id = None
+    endpoint = None
+
+    def setUp(self):
+        config = Cs3ConfigManager.get_config()
+        self.client_id = config['client_id']
+        self.endpoint = config['endpoint']
+
+
+        self.user_api = Cs3UserApi(self.log)
+
+    def test_get_user_info(self):
+        opaque_id = "4c510ada-c86b-4815-8820-42cdf82c3d51"
+        idp = "cernbox.cern.ch"
+
+        expected_info = {"username": "einstein",
+                    "display_name": "Albert Einstein",
+                    "idp": "cernbox.cern.ch",
+                    "opaque_id": "4c510ada-c86b-4815-8820-42cdf82c3d51",
+                    "mail": "einstein@cern.ch"}
+
+        user_info = self.user_api.get_user_info(idp, opaque_id)
+
+        self.assertDictEqual(user_info, expected_info)
+
+    def test_get_user_info_non_existing_id(self):
+        opaque_id = "non-existing"
+        idp = "cernbox.cern.ch"
+
+        user_info = self.user_api.get_user_info(idp, opaque_id)
+
+        self.assertFalse(user_info)
+
+    def test_get_user_by_claim(self):
+        claim = "eintein"
+        value = "cernbox.cern.ch"
+
+    def test_find_users_by_query(self):
+        query = 'einstein'
+
+        expected_info = {"username": "einstein",
+                    "display_name": "Albert Einstein",
+                    "idp": "cernbox.cern.ch",
+                    "opaque_id": "4c510ada-c86b-4815-8820-42cdf82c3d51",
+                    "mail": "einstein@cern.ch"}
+
+        user = self.user_api.find_users_by_query(query)
+        self.assertTrue(user)
+
+        user_info = user[0]
+        self.assertDictEqual(user_info, expected_info)
+
+    def test_find_users_by_query_less_than_three_chars(self):
+        query = 'e'
+        self.assertFalse(self.user_api.find_users_by_query(query))
+
+        query = 'ei'
+        self.assertFalse(self.user_api.find_users_by_query(query))
+
+    def test_find_users_by_query_three_chars(self):
+        query = 'ein'
+
+        expected_info = {"username": "einstein",
+                    "display_name": "Albert Einstein",
+                    "idp": "cernbox.cern.ch",
+                    "opaque_id": "4c510ada-c86b-4815-8820-42cdf82c3d51",
+                    "mail": "einstein@cern.ch"}
+
+        user = self.user_api.find_users_by_query(query)
+        self.assertTrue(user)
+
+        user_info = user[0]
+        self.assertDictEqual(user_info, expected_info)
+
