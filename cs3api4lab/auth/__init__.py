cccd1a06c825a3c80e3677abab3697197c590bce
Diogo Castro <diogo.castro@cern.ch>
2023-11-02 16:05:19 +0100

    Machine auth

--- cs3api4lab/auth/__init__.py
+++ cs3api4lab/auth/__init__.py
@@ -1,4 +1,5 @@
 from cs3api4lab.auth.eos import Eos
 from cs3api4lab.auth.oauth import Oauth
 from cs3api4lab.auth.reva_password import RevaPassword
+from cs3api4lab.auth.machine import Machine
 from cs3api4lab.auth.check_auth_interceptor import CheckAuthInterceptor
---
+++ cs3api4lab/auth/machine.py
@@ -0,0 +1,10 @@
+from cs3api4lab.auth.authenticator import Authenticator
+
+
+class Machine(Authenticator):
+
+    def __init__(self, config=None, log=None):
+        super().__init__(config, log)
+
+    def refresh_token(self, client_id=None, client_secret_or_token=None):
+        self.token = self._auth_in_iop(self.config.client_secret, "machine")
